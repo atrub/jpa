55adf9d1bcc822444154411bd08553b5d8e1f2c1
admin <a_trub@mail.ru>
2018-08-24 10:17:50 +0500

    commit

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,10 +1,25 @@
 package hello;
 
+import hello.domain.Employee;
+import hello.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	private EmployeeRepository employee;
+
+	@Override
+	public void run(String... args) throws Exception {
+		this.employee.save(new Employee("Igor", "Akulov", 55));
+		this.employee.save(new Employee("Daria", "Akulova", 11));
+		this.employee.save(new Employee("Alex", "Akulov", 20));
+		this.employee.save(new Employee("Margarita", "Akulova", 24));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
